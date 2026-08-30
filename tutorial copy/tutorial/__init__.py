cb2719dd6a994ba1298176ff264fb74a3be420d6
Dylan Strnad <dstrnad1@kent.edu>
2026-08-30 12:05:37 -0400

    made web page using flask

--- tutorial copy/tutorial/__init__.py
+++ tutorial copy/tutorial/__init__.py
@@ -5,7 +5,9 @@ from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 from spiders.collect_steam_games import InfinitePageSpider
 
-from flask import Flask
+from flask import Flask, render_template
+from flask import request
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -18,9 +20,52 @@ REVIEW_SCORES_INDEX = {
     "Overwhelmingly Negative": 0, "Very Negative": 1, "Negative": 2, "Mostly Negative": 3, "Mixed": 4, "Mostly Positive": 5, "Positive": 6, "Very Positive": 7, "Overwhelmingly Positive": 8
 }
 
-@app.route("/")
-def get_user_input():
-    return "<p> flask app is working?! </p>"
+def crawl_page(genre, scrape_amount, review_score):
+    print("crawl page")
+    # tag_id, scrape_amount, review_score = get_user_input()
+    # create web crawler
+    process = CrawlerProcess(
+        settings={
+            "FEEDS": {
+                "games.json": {
+                    "format": "json",
+                    "overwrite": True,
+                },
+            },
+        }
+    )
+    # pass user defined arguments
+    process.crawl(InfinitePageSpider, tag_id=genre, scrape_amount=scrape_amount, review_score=review_score)
+    process.start()
+    print("Crawl finished")
+
+def clean_data_and_embed():
+    with open("games.json") as file:
+        data = json.load(file)
+
+    cleaned_data = [item for item in data if item.get("game")]
+
+    game_descriptions = [
+        f"Title: {g['game'].strip()}. Genres: {', '.join(g['genre'])}. Tags: {', '.join(g['tags'])}. Reviews: {g['reviews']}."
+        for g in cleaned_data
+    ]
+
+    game_embeddings = model.encode(game_descriptions)
+    return game_embeddings, cleaned_data
+
+@app.route("/",methods=["GET","POST"])
+def home():
+    print("home")
+    if request.method == "POST":
+        genre = request.form.get("genre")
+        scrape_amount = request.form.get("scrape_amount")
+        review_score = request.form.get("review_score")
+        #game_description = request.form.get("game_description")
+        print("crawler")
+        crawl_page(genre, scrape_amount, review_score)
+        clean_data_and_embed()
+    return render_template('index.html')
+    return f"Hello, {escape(name)}!"
     tag_id = None
     while tag_id is None:
         genre = input(
@@ -59,51 +104,26 @@ def get_user_input():
     return tag_id, scrape_amount, review_score
 
 
-# def crawl_page():
-#     tag_id, scrape_amount, review_score = get_user_input()
-#     # create web crawler
-#     process = CrawlerProcess(
-#         settings={
-#             "FEEDS": {
-#                 "games.json": {
-#                     "format": "json",
-#                     "overwrite": True,
-#                 },
-#             },
-#         }
-#     )
-#     # pass user defined arguments
-#     process.crawl(InfinitePageSpider, tag_id=tag_id, review_score=review_score, scrape_amount=scrape_amount)
-#     process.start()
-
-# def clean_data_and_embed():
-#     with open("games.json") as file:
-#         data = json.load(file)
-
-#     cleaned_data = [item for item in data if item.get("game")]
-
-#     game_descriptions = [
-#         f"Title: {g['game'].strip()}. Genres: {', '.join(g['genre'])}. Tags: {', '.join(g['tags'])}. Reviews: {g['reviews']}."
-#         for g in cleaned_data
-#     ]
-
-#     game_embeddings = model.encode(game_descriptions)
-#     return game_embeddings, cleaned_data
+@app.route("/results.html",methods=["GET","POST"])
+def results():
+    return render_template('results.html')
 
 
-# def recommend_games(user_input, num_of_results, game_embeddings, cleaned_data):
-#     input_embedding = model.encode([user_input])
-#     calc_similarity = cosine_similarity(input_embedding, game_embeddings)[0]
 
-#     #fetches the top results
-#     top_matches = calc_similarity.argsort()[::-1][:num_of_results]
-#     for idx in top_matches:
-#         game = cleaned_data[idx]
-#         score = calc_similarity[idx]
-#         print(f"• {game['game'].strip()} (Match Score: {score:.2f})")
-#         print(f"  Genres: {', '.join(game['genre'])}")
-#         print(f"  tags: {', '.join(game['tags'])}")
-#         print(f"  Reviews: {game['reviews']}\n")
+@app.route("/results.html",methods=["GET","POST"])
+def recommend_games(game_description, scrape_amount, game_embeddings, cleaned_data):
+    input_embedding = model.encode([game_description])
+    calc_similarity = cosine_similarity(input_embedding, game_embeddings)[0]
+
+    #fetches the top results
+    top_matches = calc_similarity.argsort()[::-1][:scrape_amount]
+    for idx in top_matches:
+        game = cleaned_data[idx]
+        score = calc_similarity[idx]
+        print(f"• {game['game'].strip()} (Match Score: {score:.2f})")
+        print(f"  Genres: {', '.join(game['genre'])}")
+        print(f"  tags: {', '.join(game['tags'])}")
+        print(f"  Reviews: {game['reviews']}\n")
 
 # if __name__ == "__main__":
 #     while True:
